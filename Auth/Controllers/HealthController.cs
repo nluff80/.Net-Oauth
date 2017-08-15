2e386ff563ba726cd013d35700d451c3cae7e267
Neil <neil.luff@bluecieloecm.com>
2017-08-15 17:39:50 +0200

    Solution Creation
    Initial WebAPI project and OWIN config

---
+++ Auth/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Auth.Controllers
+{
+    public class HealthController : ApiController
+    {
+        [Route("api/v1/health")]
+        public IHttpActionResult Get()
+        {
+            return Ok("running");
+        }
+    }
+}
---
+++ Auth/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Auth.Config;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Auth.Startup))]
+
+namespace Auth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            WebApiConfig.Register(configuration);
+            app.UseWebApi(configuration);
+        }
+    }
+}
